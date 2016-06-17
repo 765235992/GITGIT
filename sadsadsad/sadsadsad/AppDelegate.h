3c869c16df2d1308eca38ae88be7d35483d26079
765235992 <765235992@qq.com>
2016-06-17 10:38:13 +0800

    ADD

---
+++ sadsadsad/sadsadsad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sadsadsad
+//
+//  Created by Jim on 16/6/17.
+//  Copyright © 2016年 Demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sadsadsad/sadsadsad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sadsadsad
+//
+//  Created by Jim on 16/6/17.
+//  Copyright © 2016年 Demon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
